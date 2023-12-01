276ca6626e79cc0b0314bab14268049b27720920
rashneh <152456790+rashneh@users.noreply.github.com>
2023-11-30 22:44:45 -0600

    update vars portfolio.java

--- app/src/main/java/com/example/mockinvestor/portfolio.java
+++ app/src/main/java/com/example/mockinvestor/portfolio.java
@@ -5,14 +5,10 @@ public class Portfolio {
 
     private int portfolioSize = 0;
     private int[] shares = new int[100]; // Assuming a maximum of 100 shares in the portfolio
-    private StockObj[] portfolio = new StockObj[100]; // Assuming a maximum of 100 stocks in the portfolio
+    private StockObj[] prtfl = new StockObj[100]; // Assuming a maximum of 100 stocks in the portfolio
 
     private float holdingPeriodReturn; // Holding period return: total return of a portfolio over user tenure (saved as percentage)
     private float averageReturn; // Average return of your portfolio per annum
-
-    public Portfolio(String name) {
-        investorName = name;
-    }
 
     public void setInvestorName(String name) {
         investorName = name;
@@ -20,7 +16,7 @@ public class Portfolio {
 
     public void buy(StockObj stock, int numShares) {
         shares[portfolioSize] = numShares;
-        portfolio[portfolioSize] = stock.multiplyByShares(numShares);
+        prtfl[portfolioSize] = stock.multiplyByShares(numShares);
         portfolioSize++;
         cash -= stock.returnMarketValue();
     }
@@ -28,17 +24,17 @@ public class Portfolio {
     public void sell(StockObj stock, int numShares) {
         cash += stock.returnMarketValue();
         int count = 0;
-        while (!stock.getSymbol().equals(portfolio[count].getSymbol())) {
+        while (!stock.getSymbol().equals(prtfl[count].getSymbol())) {
             if (count == portfolioSize) {
                 break;
             }
             count++;
         }
         while (count < portfolioSize - 1) {
-            portfolio[count] = portfolio[count + 1];
+            prtfl[count] = prtfl[count + 1];
             count++;
         }
-        portfolio[portfolioSize - 1] = null;
+        prtfl[portfolioSize - 1] = null;
         portfolioSize--;
     }
 }
