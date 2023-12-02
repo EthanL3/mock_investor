886d4a191265adc28ba65798768a1c4e1c142aec
rashneh <152456790+rashneh@users.noreply.github.com>
2023-12-02 14:11:04 -0600

    stock.java changes
    updateShares() updates number of shares.
    Will be called in purchaseShares in MainActivity
    
    T_valueHistory will be returned by function in main activity for graphs later

--- app/src/main/java/com/example/mockinvestor/Stock.java
+++ app/src/main/java/com/example/mockinvestor/Stock.java
@@ -1,40 +1,116 @@
-package com.example.mockinvestor;
+example.mockinvestor;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+import java.util.Calendar;
 
 public class Stock implements Serializable {
+    //every field marked by T_ is value considering TOTAL SHARES
     private String symbol, companyName;
-    private int quantity;
-    private double gainLossDollars, gainLossPercent, marketCap, volume;
-    private String purchaseDate, purchaseTime, currentDate, currentTime;
-    private double purchasePrice, purchaseValue, currentPrice, currentValue;
-    //Price is price per share, value is price * quantity, gainLoss is currentValue - purchaseValue
+    private double marketCap, volume;
+    private double T_gainLossDollars, T_gainLossPercent;
+    private double currentVal, purchaseVal;
+    private double T_currentVal, T_purchaseVal, T_valHistory[];
+    private int shares, count_day = 0;
+    private Date purchaseDate, currentDate;
+    private String date = "08/01/2023"; //placeholder
 
-    public Stock(String symbol, int quantity){
+    public Stock(String symbol, int shares) { //temporary code
         this.symbol = symbol;
-        this.companyName = "Company name"; //placeholder
-
-        this.purchaseDate = "01/01/2021"; //placeholder
-        this.purchaseTime = "12:00"; //placeholder
-        this.currentDate = "05/01/2021"; //placeholder
-        this.currentTime = "12:00"; //placeholder
-
-        this.quantity = quantity;
-        this.currentPrice = 150; //placeholder
-        this.currentValue = quantity * currentPrice; //placeholder
-
-        //maybe will add purchaseQuantity and currentQuantity later, but for now assume they are the same
-        this.purchasePrice = 100; //placeholder
-        this.purchaseValue = quantity * purchasePrice; //placeholder
-
-        this.gainLossDollars = currentValue - purchaseValue;
-        this.gainLossPercent = (currentValue - purchaseValue) / purchaseValue;
-
-        this.marketCap = 1000; //placeholder
-        this.volume = 1000; //placeholder
+        this.shares = shares;
     }
 
+    public Stock(String symbol, String companyName, String price, String volume, String marketCap){
+        this.symbol = symbol;
+        this.companyName = companyName; //will add when I add CSV file
+        try {
+            this.currentVal = Double.parseDouble(price);
+            this.purchaseVal = Double.parseDouble(price);
+            this.volume = Double.parseDouble(volume); //will add when I add CSV file
+            this.marketCap = Double.parseDouble(marketCap); //will add when I add CSV file
+        } catch (NumberFormatException e){
+            System.out.println("Error: initialization: string not in number format");
+        }
+        updateGainsLoss();
+    }
+    //getters for each variable: to return strings only
+    public String getSymbol(){
+        return symbol;
+    }
+    public String getCompanyName(){
+        return companyName;
+    }
+    public int getQuantity(){ //number of shares
+        return shares;
+    }
+    public double getMarketCap() {
+        return marketCap;
+    }
+    public double getVolume() {
+        return volume;
+    }
+    public double getCurrentValue(){ //per share
+        return currentVal;
+    }
+    public double getPurchaseValue() { //per share
+        return purchaseVal;
+    }
+    public double getTOTALCurrentValue(){ //returns total purchase value (x num shares)
+        return T_currentVal;
+    }
+    public double getTOTALPurchaseValue() { //returns total purchase value (x num shares)
+        return T_purchaseVal;
+    }
+    public double getGainLossDollars(){
+        return T_gainLossDollars;
+    }
+    public double getGainLossPercent(){
+        return T_gainLossPercent;
+    }
+
+    //update day function: to be used every day through clock-imitating loop
+    public void updateDay(String price){
+        try {
+            this.currentVal = Double.parseDouble(price);
+        } catch (NumberFormatException e){
+            System.out.println("Error: update string not in number format");
+        }
+        this.T_currentVal = (double) currentVal * shares;
+        count_day++;
+        this.T_valHistory[count_day] = T_currentVal;
+        updateGainsLoss();
+
+        //update date by one day in "currentDate"
+        Calendar cal_currentDate = Calendar.getInstance();
+        cal_currentDate.setTime(currentDate);
+        cal_currentDate.add(Calendar.DATE, 1);
+        currentDate = cal_currentDate.getTime();
+    }
+    //purchase stock function, to be accessed from portfolio
+    public void updateShares(int numShares){
+        this.shares = numShares;
+        SimpleDateFormat date_format = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            purchaseDate = date_format.parse(date);
+            currentDate = date_format.parse(date);
+        } catch (ParseException e) {
+            System.out.println("Error: purchaseStock: string not in MM/dd/yyyy format" + e.getMessage());
+        }
+        this.T_currentVal = (double) shares * currentVal;
+        this.T_purchaseVal = (double) shares * purchaseVal;
+        count_day++;
+        this.T_valHistory[count_day] = T_currentVal;
+        updateGainsLoss();
+    }
+
+    //private functions to be used within class (IGNORE)
+    private void updateGainsLoss(){
+        T_gainLossDollars = T_currentVal - T_purchaseVal;
+        T_gainLossPercent = (double) (T_gainLossDollars/T_purchaseVal) - 1; //saves float btw 0-1
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -49,148 +125,19 @@ public class Stock implements Serializable {
         return Objects.hash(symbol);
     }
 
-    public void sellShares(int sharesSold) {
-        if (this.quantity== sharesSold) {
-            MyApplication.getInstance().removeStockFromList(this);
-        }
-        else {
-            this.quantity -= sharesSold;
-            this.currentValue = quantity * currentPrice;
-            this.gainLossDollars = currentValue - purchaseValue;
-            this.gainLossPercent = (currentValue - purchaseValue) / purchaseValue;
-        }
-    }
-
-    public void buyShares(int sharesBought) {
-        this.quantity += sharesBought;
-        this.currentValue = quantity * currentPrice;
-        this.gainLossDollars = currentValue - purchaseValue;
-        this.gainLossPercent = (currentValue - purchaseValue) / purchaseValue;
-    }
-
-    private void updateGainLossDollars(){
-        this.gainLossDollars = currentValue - purchaseValue;
-    }
-
-    public void updateGainLossPercent(){
-        this.gainLossPercent = (currentValue - purchaseValue) / purchaseValue;
-    }
-
-
-    //SETTERS AND GETTERS:
-    public double getGainLossPercent() {
-        return gainLossPercent;
-    }
-
-    public void setGainLossPercent(double gainLossPercent) {
-        this.gainLossPercent = gainLossPercent;
-    }
-
-    public double getGainLossDollars(){
-        return gainLossDollars;
-    }
-
-    public void setGainLossDollars(double gainLossDollars){
-        this.gainLossDollars = gainLossDollars;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
+    /*public void setSymbol(String symbol){
         this.symbol = symbol;
     }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
+    public void setCompanyName(String companyName){
         this.companyName = companyName;
     }
-
-    public int getQuantity() {
-        return quantity;
+    public void setQuantity(String quantity){
+        this.quantity = quantity;
     }
-
-    public double getMarketCap() {
-        return marketCap;
+    public void setCurrentValue(String currentValue){
+        this.currentVal = currentValue;
     }
-
-    public void setMarketCap(double marketCap) {
-        this.marketCap = marketCap;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(double volume) {
-        this.volume = volume;
-    }
-
-    public String getPurchaseDate() {
-        return purchaseDate;
-    }
-
-    public void setPurchaseDate(String purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
-
-    public String getPurchaseTime() {
-        return purchaseTime;
-    }
-
-    public void setPurchaseTime(String purchaseTime) {
-        this.purchaseTime = purchaseTime;
-    }
-
-    public String getCurrentDate() {
-        return currentDate;
-    }
-
-    public void setCurrentDate(String currentDate) {
-        this.currentDate = currentDate;
-    }
-
-    public String getCurrentTime() {
-        return currentTime;
-    }
-
-    public void setCurrentTime(String currentTime) {
-        this.currentTime = currentTime;
-    }
-
-    public double getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public void setPurchasePrice(double purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
-
-    public double getPurchaseValue() {
-        return purchaseValue;
-    }
-
-    public void setPurchaseValue(double purchaseValue) {
-        this.purchaseValue = purchaseValue;
-    }
-
-    public double getCurrentPrice() {
-        return currentPrice;
-    }
-
-    public void setCurrentPrice(double currentPrice) {
-        this.currentPrice = currentPrice;
-    }
-
-    public double getCurrentValue() {
-        return currentValue;
-    }
-
-    public void setCurrentValue(double currentValue) {
-        this.currentValue = currentValue;
-    }
+    public void setGainLoss(String gainLoss){
+        this.gainLoss = gainLoss;
+    }*/
 }
