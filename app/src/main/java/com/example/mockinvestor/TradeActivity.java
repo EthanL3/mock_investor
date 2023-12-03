248a797cdbba19a54c4ef1e1dbaff89cf760ba08
rashneh <152456790+rashneh@users.noreply.github.com>
2023-12-02 21:46:46 -0600

    Update TradeActivity.java

--- app/src/main/java/com/example/mockinvestor/TradeActivity.java
+++ app/src/main/java/com/example/mockinvestor/TradeActivity.java
@@ -12,7 +12,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class TradeActivity extends AppCompatActivity {
+
     ArrayList<Stock> added_stocks = new ArrayList<>();
+
+    double holdings = 0, cash = 100000;
+    int numStocks = 0;
+
+    public int getNumStocks(){ return numStocks; }
+    public void purchaseStocks(Stock stock, int shares) {
+        int count = 0;
+        while (count <= numStocks) {
+            if (added_stocks.get(count).getSymbol() == stock.getSymbol()) { //add shares if stock already in portfolio
+                added_stocks.get(count).updateShares((added_stocks.get(count).getQuantity() + shares));
+                break;
+            }
+            count++;
+        }
+        if (count > numStocks) { //if stock does not already exist in portfolio
+            added_stocks.get(count).updateShares(shares);
+        }
+        cash = cash - added_stocks.get(count).getTOTALCurrentValue();
+        holdings = holdings + added_stocks.get(count).getTOTALCurrentValue();
+        numStocks++;
+    }
+    public void setStocksList(ArrayList<Stock> stocks){
+        this.added_stocks = stocks;
+    }
+    public void sellStocks(Stock stock, int shares) {
+        int count = 0;
+        int shares_buffer;
+        while (count <= numStocks) {
+            if (added_stocks.get(count).getSymbol() == stock.getSymbol()) { //add shares if stock already in portfolio
+                shares_buffer = added_stocks.get(count).getQuantity() - shares;
+                if (shares_buffer < 1) {
+                    added_stocks.remove(count);
+                } else {
+                    added_stocks.get(count).updateShares(shares_buffer);
+                }
+                break;
+            } //if stock is found in portfolio bracket
+            count++;
+        }
+        if (count > numStocks) { //if stock does not already exist in portfolio
+            System.out.println("Error: This stock does not exist in your portfolio");
+        }
+        cash = cash + added_stocks.get(count).getTOTALCurrentValue();
+        holdings = holdings - added_stocks.get(count).getTOTALCurrentValue();
+        numStocks--;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +68,10 @@ public class TradeActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Trade");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        TextView total_money_left = findViewById(R.id.total_money_left);
+        //TextView total_money_left = findViewById(R.id.total_money_left);
 
         //Money left to spend, placeholder for now
-        total_money_left.setText("Total money left: $100000");
+        //total_money_left.setText("Total money left: $100000");
 
 
         Button btnBuy = findViewById(R.id.btnBuy);
@@ -41,7 +89,7 @@ public class TradeActivity extends AppCompatActivity {
                 if (MyApplication.getInstance().containsStock(user_stock)) {
                     int stockIndex = MyApplication.getInstance().getAllUserStocks().indexOf(user_stock);
                     Stock stock = MyApplication.getInstance().getAllUserStocks().get(stockIndex);
-                    stock.buyShares(shares);
+                    purchaseStocks(user_stock,shares);
                 }
                 //else add the stock to the list
                 else {
