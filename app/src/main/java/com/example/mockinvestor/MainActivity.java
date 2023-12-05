86b7db64e6f0c804c886a511964f6bfdc6385e3d
Rashne Hassan <77666667+rhassan895@users.noreply.github.com>
2023-12-04 21:00:24 -0600

    initialize

--- app/src/main/java/com/example/mockinvestor/MainActivity.java
+++ app/src/main/java/com/example/mockinvestor/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         //if user has stocks, display them
         if(!MyApplication.getInstance().getAllUserStocks().isEmpty()) {
             MyApplication.getInstance().loadStocksAtOpen();
-            MyApplication.getInstance().updateStockData();
             stocks = MyApplication.getInstance().getAllUserStocks();
         }
 
